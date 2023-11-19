2229d3fdfa9d5467c1a8ca6855ca6d18be664267
HodakovaNA <144614378+HodakovaNA@users.noreply.github.com>
2023-11-19 18:28:18 +0500

    Task1 V1

---
+++ Tyuiu.HodakovNA.Sprint5.Task1.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.HodakovNA.Sprint5.Task1.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.HodakovNA.Sprint5.Task1.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61305a1f-57db-4e80-80bf-31de7ed8ca46")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.HodakovNA.Sprint5.Task1.V1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.HodakovNA.Sprint5.Task1.V1.Lib;
+
+namespace Tyuiu.HodakovNA.Sprint5.Task1.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Ходакова Н.А. | АСОиУб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка файлов                                                  *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант 1                                                               *");
+            Console.WriteLine("* Выполнила: Ходакова Надежда Антоновна | АСОиУб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано функция, произвести табуляцию на диапазоне [-5;5] с шагом 1.       *");
+            Console.WriteLine("* Произвести проверку деления на 0. При делении на ноль вернуть значение  *");
+            Console.WriteLine("* 0. Результат сохранить в текстовый файл OutPutFileTask0.txt и вывести   *");
+            Console.WriteLine("* на консоль в таблицу. Значения окрулить до двух знаков после запятой.   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            
+            int startValue = -5;
+            int stopValue = 5;
+            
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            int count = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                valueArray[count] = ((5 * x + 2.5) / (Math.Sin(x) + 2) + 2 * x + 2);
+                count++;
+            }
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |    f(x)  |");
+            Console.WriteLine("+----------+----------+");
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     | {1,6:f2}   |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+
+            Console.ReadLine();
+        }
+    }
+}
