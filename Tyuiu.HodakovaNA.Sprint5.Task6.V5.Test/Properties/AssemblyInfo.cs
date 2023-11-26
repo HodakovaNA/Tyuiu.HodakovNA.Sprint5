c121f55c3f1d925f49f5c99328b3c75bda6422cc
HodakovaNA <144614378+HodakovaNA@users.noreply.github.com>
2023-11-26 19:38:26 +0500

    Task6 V5

---
+++ Tyuiu.HodakovaNA.Sprint5.Task6.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.HodakovaNA.Sprint5.Task6.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.HodakovaNA.Sprint5.Task6.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a2a88cd-7569-47fa-b8ae-ccad320d81bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
